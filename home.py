f741336ff4b4243761b04eb237f5116f4a010fac
Charlotte Swift <charlotteswift@charlottesmbp4.home>
2017-04-02 16:47:10 +0100

    First commit

--- home.py
+++ home.py
@@ -6,30 +6,19 @@
 
 from flask import Flask, request, render_template
 import requests
+from urllib import urlencode
 #this is way you are able to import different modules and libraries to use
 app = Flask("safe_spot")
 
-@app.route("/home")
+@app.route("/")
 #this defines a route for the default page when you get to this part in your app url...run the following
-def home(name):
-    return render_template("home.html" name=name.title())
+def hello():
+    return render_template("home.html")
 
-#user to enter their location here. work out how user can enter their address
-def user_spot(x):
-    print "So, whereabouts are you in the world?"
-    print "You are here: %r, and you are about to find your safe spot" %(x)
-
-#will then transfer over to the next page...
-
-@app.route("/welcome")
-#when you get to this part in your app...run the following
-def welcome():
-    return render_template("welcome.html")
-
-def user_spot(x):
-    print "So, whereabouts are you in the world?"
-    print "You are here: %r, and you are about to find your safe spot" %(x)
-
+@app.route("/<name>")
+#this defines a route for the default page when you get to this part in your app url...run the following
+def home_someone(name):
+    return render_template("home.html", name=name.title())
 
 #to enter the user_spot
 @app.route("/maps.googleapis.com/maps/api/staticmap?center=<user_spot>&size=640x400&style=element:labels|visibility:off&style=element:geometry.stroke|visibility:off&style=feature:landscape|element:geometry|saturation:-100&style=feature:water|saturation:-100|invert_lightness:true&key=AIzaSyAGCXXvra_pUEjBTEz92VUgAbno-8L4o9c", methods=['POST'])
@@ -42,11 +31,15 @@ def user_spot():
 #to return the google static url with chosen place in
     return "All OK"
 
-def hello_someone(name):
-    return render_template("hello.html", name=name.title())
 
-#set up the email
+def user_spot_map(Place):
+    #how to take the user address, and return it in the address?
+    return render_template("welcome.html", Place=Place.title())
 
+
+#-----
+
+#set up the email contact
 def send_simple_message(email):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox61044a42a32a43f1851ce3532eba9064.mailgun.org/messages",
